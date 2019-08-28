a7e2b2f38923a69b611f0ba31b7d2a422e8d5b4c
Paul Makepeace <paulm@paulm.com>
2019-08-27 20:21:29 -0700

    Factor out IEEE Reg Auth into ieee_ra.py

--- descrypi.py
+++ descrypi.py
@@ -9,23 +9,14 @@ We restrict `arp` usage to the BSD format so that it works on macOS and Linux
 without getting into platform switching.
 """
 
-import csv
-import io
+import json
 import re
 import subprocess
-import textwrap
-import urllib.parse
-import urllib.request
+
+import ieee_ra
 
 # -a shows the current table. -n skips reverse DNS lookup (no use for name)
 ARP_COMMAND = "arp -n -a".split()
-
-# Known RPi MAC prefixes, sorted
-RASPBERRY_PI_MACS = ['B8:27:EB', 'DC:A6:32']
-
-# URL to query the IEEE MAC Registration Authority for updated MAC prefixes
-IEEE_RA_URL = "https://services13.ieee.org/RST/standards-ra-web/rest/assignments/download/"
-IEEE_RA_QUERY = '"raspberry pi"'
 
 # There are two `arp -a` outputs here, Linux and macOS. Fortunately the
 # interesting part is the same.
@@ -37,7 +28,7 @@ ARP_LINE_RE = re.compile(
 
 # Regex to filter RPi MACs
 RASPBERRY_PI_MACS_RE = re.compile(
-    r'^(' + "|".join(RASPBERRY_PI_MACS) + r'):',
+    r'^(' + "|".join(ieee_ra.RASPBERRY_PI_MACS) + r'):',
     re.IGNORECASE
 )
 
@@ -55,38 +46,12 @@ def filter_ip_macs(ip_macs, filter_re=RASPBERRY_PI_MACS_RE):
   """Apply the filter_re regex to the MAC address and return matching subset."""
   return [(ip, mac) for (ip, mac) in ip_macs if filter_re.search(mac)]
 
-def ieee_ra_url(query):
-  """Return a URL to query IEEE Registration Authority's MAC database."""
-  # curl "https://services13.ieee.org/RST/standards-ra-web/rest/assignments/download/?registry=MAC&text=%22raspberry%20pi%22"
-  params = urllib.parse.urlencode({'registry': 'MAC', 'text': query})
-  return IEEE_RA_URL + "?" + params
-
-def macs_from_ieee_ra(query=IEEE_RA_QUERY):
-  """Query IEEE Reg Authority for RPi MAC.
-
-  Data looks like,
-  Registry,Assignment,Organization Name,Organization Address
-  MA-L,DCA632,Raspberry Pi Trading Ltd,"Maurice Wilkes Building, Cowley Road Cambridge  GB CB4 0DS "
-  MA-L,B827EB,Raspberry Pi Foundation,Mitchell Wood House Caldecote Cambridgeshire US CB23 7NU
-  """
-  try:
-    with urllib.request.urlopen(ieee_ra_url(query)) as f:
-      r = csv.reader((line.decode() for line in f), delimiter=',', quotechar='"')
-      next(r) # skip header
-      # Split "01AB23" into "01:AB:23"
-      return sorted((":".join(textwrap.wrap(row[1], 2)) for row in r))
-  except urllib.error.HTTPError as err:
-    print("IEEE RA query failed:", err, "(may be transient; try again?)")
-    return []
-
-def verify_ieee_macs(query=IEEE_RA_QUERY, macs=RASPBERRY_PI_MACS):
-  """Ensure no new MAC prefixs have popped up."""
-  return macs == macs_from_ieee_ra(query)
-
 def print_ip_macs():
   """Main entry point."""
   print("IP Macs of Pis:")
-  print(filter_ip_macs(find_ip_macs()))
+  ip_macs = filter_ip_macs(find_ip_macs())
+  print(json.dumps(ip_macs))
 
 if __name__ == '__main__':
   print_ip_macs()
+  print(ieee_ra.macs_from_ieee_ra())
---
+++ ieee_ra.py
@@ -0,0 +1,47 @@
+"""Query the IEEE Registration Authority for MAC prefixes.
+
+The Raspberry Pi folks have two MAC prefixes. It's possible in the future
+they'll add another. This file demonstrates how to query the IEEE RA database
+for those MAC prefixes.
+"""
+
+import csv
+import textwrap
+import urllib.parse
+import urllib.request
+
+# Known RPi MAC prefixes, sorted
+RASPBERRY_PI_MACS = ['B8:27:EB', 'DC:A6:32']
+
+# URL to query the IEEE Registration Authority for updated MAC prefixes
+IEEE_RA_URL = "https://services13.ieee.org/RST/standards-ra-web/rest/assignments/download/"
+IEEE_RA_QUERY = '"raspberry pi"'
+
+def ieee_ra_url(query):
+  """Return a URL to query IEEE Registration Authority's MAC database."""
+  # curl "https://services13.ieee.org/RST/standards-ra-web/rest/assignments/download/?registry=MAC&text=%22raspberry%20pi%22"
+  params = urllib.parse.urlencode({'registry': 'MAC', 'text': query})
+  return IEEE_RA_URL + "?" + params
+
+def macs_from_ieee_ra(query=IEEE_RA_QUERY):
+  """Query IEEE Reg Authority for RPi MAC.
+
+  Data looks like,
+  Registry,Assignment,Organization Name,Organization Address
+  MA-L,DCA632,Raspberry Pi Trading Ltd,"Maurice Wilkes Building, Cowley Road Cambridge  GB CB4 0DS "
+  MA-L,B827EB,Raspberry Pi Foundation,Mitchell Wood House Caldecote Cambridgeshire US CB23 7NU
+  """
+  try:
+    with urllib.request.urlopen(ieee_ra_url(query)) as f:
+      r = csv.reader((line.decode() for line in f), delimiter=',', quotechar='"')
+      next(r) # skip header
+      # Split "01AB23" into "01:AB:23"
+      return sorted((":".join(textwrap.wrap(row[1], 2)) for row in r))
+  except urllib.error.HTTPError as err:
+    print("IEEE RA query failed:", err, "(may be transient; try again?)")
+    return []
+
+def verify_ieee_macs(query=IEEE_RA_QUERY, macs=RASPBERRY_PI_MACS):
+  """Ensure no new MAC prefixs have popped up."""
+  return macs == macs_from_ieee_ra(query)
+
