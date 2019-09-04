8480492098e840fc646aa6fb72d0a5a3029165c9
Paul Makepeace <paulm@paulm.com>
2019-09-03 23:33:19 -0700

    Allow ssh to accept stdin data

--- descrypi/ssh.py
+++ descrypi/ssh.py
@@ -8,13 +8,18 @@ DEFAULT_SSH_USER = "pi"
 # -T disables pseudo-tty since we don't need it (non-interactive)
 SSH_COMMAND = ["ssh", "-T", "-o", "StrictHostKeyChecking=no"]
 
-def ssh(host, command):
+def ssh(host, command, stdin=None):
   """Run 'command' on 'host' via ssh.
 
   'command' can be a string or list.
   """
   process = subprocess.Popen([*SSH_COMMAND, DEFAULT_SSH_USER + "@" + host, *command],
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+                             stdin=(subprocess.PIPE if stdin else None),
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.STDOUT)
+  if stdin:
+    with process.stdin as f:
+      f.write(stdin.encode())
   output = process.stdout.read().decode()
   process.wait()
   if process.returncode != 0:
